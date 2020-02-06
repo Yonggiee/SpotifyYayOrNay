994d06a2d02aac801525f07d0e5ca9a747f21abf
Yonggiee <fongyongjie27@gmail.com>
2020-02-06 21:52:06 +0800

    split train and test dataset

--- AddColumn.py
+++ AddColumn.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-f = pd.read_csv("train.csv")
-f['isLiked'] = [1 if x >=0.75 else 0 for x in f['danceability']]
-f.to_csv("train.csv", index=False)
+def addColumn(filename, value):
+    f = pd.read_csv(filename)
+    f['isLiked'] = value
+    return f
+
+yes = addColumn("Yes.csv", 1)
+no = addColumn("No.csv", 0)
+result = pd.concat([yes, no])
+result.to_csv("train.csv", index=False)
--- SpotifyModelDecisionTree.py
+++ SpotifyModelDecisionTree.py
@@ -1,24 +1,24 @@
 import pandas as pd
 from sklearn.tree import DecisionTreeClassifier  # Import Decision Tree Classifier
-# Import train_test_split function
 from sklearn.model_selection import train_test_split
 
 songs = pd.read_csv("train.csv")
 feature_cols = ['danceability', 'energy', 'key',
                 'loudness', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence',
                 'tempo']
-                
-X = songs[feature_cols]  # Features
-y = songs['isLiked']     # Target variable
+
+X_train, X_test, y_train, y_test = train_test_split(songs, songs['isLiked'], test_size=0.33, random_state=43)
+
+X_trainFeat = X_train[feature_cols]  # Features
+X_testFeat = X_test[feature_cols]
 
 clf = DecisionTreeClassifier()
-clf = clf.fit(X, y)
+clf = clf.fit(X_trainFeat, y_train)
 
-testSet = pd.read_csv("test.csv")
-y_pred = clf.predict(testSet[feature_cols])
+y_pred = clf.predict(X_testFeat[feature_cols])
 
 #output in new csv file
-results = testSet
+results = X_test.copy()
 results['isLiked'] = y_pred
 results = results[['name', 'isLiked']]
 results.to_csv("results.csv", index=False)
